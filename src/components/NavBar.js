a795561a561a4b74699e90d0866b2b4e2c866cac
mhmttpk <mttopak@gmail.com>
2024-05-12 16:56:14 +0300

    Navbar completed and some profile images added

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,9 @@
-import Link from 'next/link'
-import { useRouter } from 'next/router'
-import React from 'react'
-import Logo from './Logo'
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import React from 'react';
+import Logo from './Logo';
+import { TwitterIcon, LinkedInIcon, GithubIcon, TrailHead, SalesForce } from './Icons';
+import { motion } from 'framer-motion';
 
 const CustomLink = ({href, title, className=""}) => {
     const router = useRouter();
@@ -29,12 +31,23 @@ const NavBar = () => {
             <CustomLink href='/articles' title="Articles" className='ml-4' />
         </nav>
         
-        <nav>
-            <Link href='/login' target={"_blank"}>T</Link>
-            <Link href='/login' target={"_blank"}>T</Link>
-            <Link href='/login' target={"_blank"}>T</Link>
-            <Link href='/login' target={"_blank"}>T</Link>
-            <Link href='/login' target={"_blank"}>T</Link>
+        <nav className='flex items-center justify-center flex-wrap'>
+            <motion.a href='https://twitter.com/topakdev' target={"_blank"}
+            whileHover={{y:-2}} whileTap={{scale:0.9}} className='w-6 mr-3'>
+                <TwitterIcon/>
+            </motion.a>
+            <motion.a href='https://linkedin.com/in/mehmet-topak-6046b3ba' target={"_blank"}
+            whileHover={{y:-2}} whileTap={{scale:0.9}} className='w-6 mx-3'>
+                <LinkedInIcon/>
+            </motion.a>
+            <motion.a href='https://github.com/mhmttpk' target={"_blank"}
+            whileHover={{y:-2}} whileTap={{scale:0.9}} className='w-6 mx-3'>
+                <GithubIcon/>
+            </motion.a>
+            <motion.a href='https://www.salesforce.com/trailblazer/mtopak' target={"_blank"}
+            whileHover={{y:-2}} whileTap={{scale:0.9}} className='w-6 ml-3 mt-2'>
+                <SalesForce/>
+            </motion.a>
         </nav>
 
         <div className='absolute left-[50%] top-2 translate-x-[-50%]'>
